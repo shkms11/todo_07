b43aa1ebcbb25693bd2e5b6a3f749990fe6d03d5
Shamsul <prog@email.com>
2024-04-30 04:47:02 +0600

    added required function in app.js

--- src/App.js
+++ src/App.js
@@ -8,12 +8,64 @@ import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
 import ListGroup from "react-bootstrap/ListGroup";
 
-function App() {
-  return (
-    <div>
-      <h1>hello</h1>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userInput: "",
+      list: [],
+    };
+  }
+
+  updateInput(value) {
+    this.setState({ userInput: value });
+  }
+
+  addItem() {
+    if (this.state.userInput !== "") {
+      const userInput = {
+        id: Math.random(),
+
+        value: this.state.userInput,
+      };
+
+      const list = [...this.state.state.list, userInput];
+
+      this.setState({
+        list,
+        userInput: "",
+      });
+    }
+  }
+
+  deleteItem(key) {
+    const list = [...this.state.list];
+    const updateList = list.filter((item) => item.id !== key);
+
+    this.setState({
+      list: updateList,
+    });
+  }
+
+  editItem = (index) => {
+    const todos = [...this.state.list];
+
+    const editedTodo = prompt("Edit the todo: ");
+    if (editedTodo !== null && editedTodo.trim() !== "") {
+      let updatedTodos = [...todos];
+      updatedTodos[index].value = editedTodo;
+      this.setState({
+        list: updatedTodos,
+      });
+    }
+  };
+  render() {
+    return (
+      <div>
+        <h1>hello</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
