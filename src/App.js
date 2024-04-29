80eff8d00c6ec15170475f731ee471f11dc0593f
Shamsul <prog@email.com>
2024-04-29 21:30:32 +0600

    Added imports in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
-import "./App.css";
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
+import ListGroup from "react-bootstrap/ListGroup";
 
 function App() {
   return (
