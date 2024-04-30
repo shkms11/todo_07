24490951f45d38fecb8dff640a98432140838065
Shamsul <prog@email.com>
2024-04-30 07:16:41 +0600

    Completed App.js

--- src/App.js
+++ src/App.js
@@ -29,7 +29,8 @@ class App extends Component {
         value: this.state.userInput,
       };
 
-      const list = [...this.state.state.list, userInput];
+      const list = [...this.state.list];
+      list.push(userInput);
 
       this.setState({
         list,
@@ -61,9 +62,80 @@ class App extends Component {
   };
   render() {
     return (
-      <div>
-        <h1>hello</h1>
-      </div>
+      <Container>
+        <Row
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            fontSize: "3rem",
+            fontWeight: "bolder",
+          }}
+        >
+          TODO LIST
+        </Row>
+        <hr />
+        <Row>
+          <Col md={{ span: 5, offset: 4 }}>
+            <InputGroup className="mb-3">
+              <FormControl
+                placeholder="add item . . ."
+                size="lg"
+                value={this.state.userInput}
+                onChange={(item) => this.updateInput(item.target.value)}
+                aria-label="add something"
+                aria-describedby="basic-addon2"
+              />
+              <InputGroup>
+                <Button
+                  variant="dark"
+                  className="mt-2"
+                  onClick={() => this.addItem()}
+                >
+                  ADD
+                </Button>
+              </InputGroup>
+            </InputGroup>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={{ span: 5, offset: 4 }}>
+            <ListGroup>
+              {this.state.list.map((item, index) => {
+                return (
+                  <div key={index}>
+                    <ListGroup.Item
+                      variant="dark"
+                      action
+                      style={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                      }}
+                    >
+                      {item.value}
+                      <span>
+                        <Button
+                          style={{ marginRight: "10px" }}
+                          variant="light"
+                          onClick={() => this.deleteItem(item.id)}
+                        >
+                          Delete
+                        </Button>
+                        <Button
+                          variant="light"
+                          onClick={() => this.editItem(index)}
+                        >
+                          Edit
+                        </Button>
+                      </span>
+                    </ListGroup.Item>
+                  </div>
+                );
+              })}
+            </ListGroup>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
